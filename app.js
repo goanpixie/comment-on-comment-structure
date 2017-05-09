8dc866c0f6e6a131df8dc5d3b9f4abadf9a3bf76
Priyanka <priyanka3007@gmail.com>
2017-05-09 01:37:35 -0700

    comment on comment structure 4 levels deep

--- app.js
+++ app.js
@@ -1,88 +1,123 @@
 var app = angular.module('plunker', []);
 
-app.controller('MainCtrl', function($scope, commentFactory ) {
-    $scope.comments = commentFactory.getAllComment();
+app.controller('MainCtrl', function($scope, commentFactory) {
+
+    var vm = this;
+
+    vm.comment
+
+    vm.getAllComment = function getAllComment() {
+        $scope.comments = commentFactory.getAllComment();
+    }
+
+    vm.postComment = function postComment() {
+        commentFactory.addParentComment({
+            message: vm.comment
+        });
+    }
+
+    vm.getAllComment();
 
 });
 
 app.component('commentComponent', {
-    template: 
-    `
-    <h1>
-    	{{commentCtrl.comment.comment}} 
-    	<button ng-click = "commentCtrl.addReply()">Reply</button>
-    </h1>
-	<ul>
-		<li ng-repeat = "subComment in commentCtrl.comment.subComments">
-			<comment-component comment= "subComment"></comment-component>
-    	</li>
-	</ul>
+    template: `
+    <div>
+    	<h2>
+			
+    		{{commentCtrl.comment.message}} 
+    		<button ng-click = "commentCtrl.toggleReplyView()">
+    			Reply
+    		</button>
+    	</h2>
+        <form ng-if = "commentCtrl.isReplyVisible" ng-submit="commentCtrl.addReply()">
+            <input ng-model="commentCtrl.subComment" /required>
+            <input type="submit" value="Post Reply">
+        </form>
+        <ul>
+        	<li ng-repeat = "subComment in commentCtrl.comment.subComments" | orderBy:'-' >
+        		<comment-component index = "commentCtrl.index" comment="subComment"></comment-component>
+        	</li>
+        </ul>
+
+    </div>
     `,
     bindings: {
-        comment: '='
+        comment: '=',
+        index: '='
     },
-    controller: function MyComponentCtrl() {
-        console.log(this.comment.subComments);
+    controller: function MyComponentCtrl(commentFactory) {
 
-        this.addReply = function () {
-        	console.log('Let add some reply to this comment', this.comment.comment);
-        }
+        Object.assign(this, {
+            isReplyVisible: false,
+            subComment: '',
+            addReply() {
+                this.index = this.index + '1';
+                var commentLevel = this.index.length;
+                if (!this.comment.subComments) this.comment.subComments = [];
+                // Check if we have reached at 4th level nesting. if so, do not do anything.
+                if (commentLevel <= 4) {
+                    this.comment.subComments = [...this.comment.subComments, { message: this.subComment }];
+                }
+                
+
+            },
+            //toggles Reply input box visibility
+            toggleReplyView() {
+                this.isReplyVisible = !this.isReplyVisible;
+            },
+
+
+        });
     },
     controllerAs: 'commentCtrl'
 });
 
 app.factory('commentFactory', function() {
-    var ParentComments = [{
-            "comment": "Suhas is idiot",
+    var allComments = [{
+            "message": "Fake news is a modern online problem ",
             "subComments": [{
-                "comment": "Couldn't agree more"
+                "message": "Fiskkit to rescue!"
             }, {
-                "comment": "Fuck off"
+                "message": "Unmonitered reader consumption"
 
             }]
         }, {
-            "comment": "Priyanka is even bigger one",
+            "message": "Love the new Oculus Rift",
             "subComments": [{
-                "comment": "She is indeed"
+                "message": "Virtual Reality is here"
             }, {
-                "comment": "No, she isn't"
+                "message": "Gaming is going to get real"
             }]
-        }
+        },
+
+        { 
+        		"message": "1", 
+        		"subComments": [{ 
+        			"message": "2",
+        			"subComments": [{
+        			  "message": "3", 
+        			  "subComments": [{ 
+        			  	"message": "4" 
+        			  }]        			  
+        			}] 
+        		}] 
+        	}
+
+        	
 
     ];
 
 
     return {
         addParentComment: function(comment) {
-            ParentComments.push(comment);
+            allComments.push(comment);
         },
 
-        getAllComment: function(callback) {
-            // callback(ParentComments);
-            console.log('I was returning');
-            return ParentComments;
-        },
-
-        addChildComment: function(data) {
-            console.log(data);
-            var parentComment = data.parentComment;
-            var childComment = data.childComment;
-
-            ParentComments = ParentComments.map(function(commentObj) {
-                if (commentObj.comment == parentComment.comment) {
-                    if (commentObj.subComments === undefined) {
-                        commentObj.subComments = [];
-                    }
-                    commentObj.subComments.push({
-                        comment: childComment
-                    });
-                }
-                return commentObj;
-            });
-            console.log(ParentComments);
-            return ParentComments;
-        },
-
-
+        getAllComment: function() {
+            return allComments;
+        }
     };
 });
+
+
